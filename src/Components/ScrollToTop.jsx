a5314f4e1f86a50d2b8eaae9b406e787695639ee
sheldonramlal <sheldon.ramlal@my.uwi.edugit config --global user.name sheldon>
2024-03-08 02:57:23 -0400

    ...

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -1,17 +1,12 @@
-import { useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-function ScrollToTop({ history }) {
+export default function ScrollToTop({children}) {
+  const { pathname } = useLocation();
+
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      window.scrollTo(0, 0);
-    });
-    return () => {
-      unlisten();
-    }
-  }, []);
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return (null);
+  return children || null;
 }
-
-export default withRouter(ScrollToTop);
