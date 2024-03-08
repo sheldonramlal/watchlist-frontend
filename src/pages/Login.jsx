cc074b198af7cbb9874abe2e117647a6c638c5b4
sheldonramlal <sheldon.ramlal@my.uwi.edugit config --global user.name sheldon>
2024-03-08 12:21:20 -0400

    updated forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,40 +15,42 @@ const Login = () => {
     }
 
     return(
-        <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+        <section className="bg-gray-50 dark:bg-gray-800">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-[90vh] lg:py-0">
       
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full  rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 bg-gray-900">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl flex justify-center font-bold leading-tight tracking-tight text-gray-500 md:text-2xl">
+              <h1 className="text-xl flex justify-center font-bold leading-tight tracking-tight text-white md:text-2xl">
                   Log in to your account
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 "> Email</label>
+                      <label  className="block mb-2 text-sm font-medium text-white "> Email</label>
                       <input type="email" 
                        className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                       placeholder="name@company.com"
+                       placeholder="name@email.com"
                        
                        onChange={(e) => setEmail(e.target.value)}
                        value={email}
                        />
                   </div>
-                  <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                  <div className="mb-5">
+                      <label  className="block mb-2 text-sm font-medium text-white ">Password</label>
                       <input type="password" 
                       className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                      
                       onChange={(e) => setPassword(e.target.value)}
                       value={password}
                       />
                   </div>
                   
-                  <button disabled={isLoading} type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 text-center">Log in</button>
+                  <div>
+
+                  <button disabled={isLoading} type="submit" className="w-full text-white bg-blue-600 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 text-center">Log in</button>
                   {error && <div className="w-full border-2 border-red-500 rounded-md bg-red-400 flex justify-center">{error}</div>}
+                  </div>
 
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Dont have an account yet? <Link to="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                      Dont have an account yet? <Link to="/signup" className="font-medium text-white">Sign up</Link>
                   </p>
               </form>
           </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -16,17 +16,17 @@ const Signup = () => {
 
     return(
         
-        <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+        <section className="bg-gray-50 dark:bg-gray-800">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-[90vh] lg:py-0">
       
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="w-full  rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 bg-gray-900">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl flex justify-center font-bold leading-tight tracking-tight text-gray-500 md:text-2xl">
+              <h1 className="text-xl flex justify-center font-bold leading-tight tracking-tight text-white md:text-2xl">
                   Sign up
               </h1>
               <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 "> Email</label>
+                      <label  className="block mb-2 text-sm font-medium text-white"> Email</label>
                       <input type="email" 
                        className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                        placeholder="name@company.com"
@@ -36,7 +36,7 @@ const Signup = () => {
                        />
                   </div>
                   <div>
-                      <label  className="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
+                      <label  className="block mb-2 text-sm font-medium text-white ">Password</label>
                       <input type="password" 
                       className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                       
@@ -45,11 +45,14 @@ const Signup = () => {
                       />
                   </div>
                   
+                  <div>
+
                   <button disabled={isLoading} type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 text-center">Sign up</button>
                   {error && <div className="w-full border-2 border-red-500 rounded-md bg-red-400 flex justify-center">{error}</div>}
+                  </div>
 
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <Link to="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Log in</Link>
+                      Already have an account? <Link to="/login" className="font-medium text-white">Log in</Link>
                   </p>
               </form>
           </div>
