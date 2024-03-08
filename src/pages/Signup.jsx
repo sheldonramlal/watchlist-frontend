78e9e6906871586f2d4d36a1169872d3521a2ace
sheldonramlal <sheldon.ramlal@my.uwi.edugit config --global user.name sheldon>
2024-03-08 12:23:48 -0400

    ...

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -16,7 +16,7 @@ const Signup = () => {
 
     return(
         
-        <section className="bg-gray-50 dark:bg-gray-800">
+        <section className="bg-gray-800">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-[90vh] lg:py-0">
       
       <div className="w-full  rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 bg-gray-900">
